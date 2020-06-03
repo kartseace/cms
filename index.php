5721162f0abc8dbb643d02a3c786b8d55a3b0460
Karma Lama <kartseace@MacBook-Air.local>
2020-06-03 15:33:31 +0545

    users update comp

--- index.php
+++ index.php
@@ -3,66 +3,69 @@ include "includes/header.php";
 ?>
 <?php include "includes/db.php";?>
 
- <!-- Navigation -->
+<!-- Navigation -->
 <?php include "includes/navigation.php"; ?>
-    <!-- Page Content -->
+<!-- Page Content -->
 <div class="container">
-  <div class="row">
+<div class="row">
 
-    <!-- Blog Entries Column -->
-     <div class="col-md-8">
-                       
-              
-        <?php
-			$query="SELECT * FROM posts";
-			$select_all_posts_query=mysqli_query($connection,$query);
-			while($row = mysqli_fetch_assoc($select_all_posts_query)){
-					$post_title=$row['post_title'];
-					$post_author=$row['post_author'];
-					$post_date=$row['post_date'];
-					$post_image=$row['post_image'];
-					$post_content=$row['post_content'];
-					
-					?>
-					
-					
-					
-               
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+<!-- Blog Entries Column -->
+<div class="col-md-8">
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"><?php echo $post_title;?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author;?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php $post_date;?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-                <hr>
-                <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
- 
-                <hr>
-                <?php
 
-				}
-				
-				?>
-               
-               
-               
+<?php
+	$query="SELECT * FROM posts";
+	$select_all_posts_query=mysqli_query($connection,$query);
+	while($row = mysqli_fetch_assoc($select_all_posts_query)){
+			$post_id=$row['post_id'];
+			$post_title=$row['post_title'];
+			$post_author=$row['post_author'];
+			$post_image=$row['post_image'];
+			$post_date=$row['post_date'];
+			$post_content=substr($row['post_content'],0,100);
+			$post_status=$row['post_status'];
+		
+		if($post_status!=='published'){
+			echo "<h1 class='text-center'>No post</h1>";
+		}
+		else{
+		
+			?>
+			
+			<h1 class="page-header">
+			Page Heading
+			<small>Secondary Text</small>
+		</h1>
 
-            </div>
+		<!-- First Blog Post -->
+		<h2>
+			<a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title;?></a>
+		</h2>
+		<p class="lead">
+			by <a href="index.php"><?php echo $post_author;?></a>
+		</p>
+		<p><span class="glyphicon glyphicon-time"></span> Posted on <?php $post_date;?></p>
+		<hr>
+		<img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+		<hr>
+		<p><?php echo $post_content;?></p>
+		<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php"; ?>
-        </div>
-        <!-- /.row -->
+		<hr>
+			
+		
+		<?php } }?>
 
-        <hr>
+
+
+
+	</div>
+
+	<!-- Blog Sidebar Widgets Column -->
+	<?php include "includes/sidebar.php"; ?>
+</div>
+</div>
+<!-- /.row -->
+
+<hr>
 <?php include "includes/footer.php";
